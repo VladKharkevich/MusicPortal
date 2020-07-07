c64644adfbe7f6e04e6b831bb02565d5fe7be523
Vlad <kharkevichvlad@gmail.com>
2020-07-08 01:37:54 +0300

    Created a 'layout' of this application

--- music_portal/urls.py
+++ music_portal/urls.py
@@ -5,8 +5,17 @@ from .views import *
 
 urlpatterns = [
     path('', MainPage.as_view(), name='main_page_url'),
-    path('news/', NewsList.as_view(), name='news_page_url'),
+    path('about/', AboutPage.as_view(), name='about_page_url'),
+    path('contacts/', ContactPage.as_view(), name='contacts_url'),
+    path('musical-instruments/', MusicalInstrumentList.as_view(),
+         name='musical_instruments_page_url'),
+    # path('musical-instruments/<str:slug>/', MusicalInstrumentDetail.as_view(),
+    #      name='musical_instruments_detail_url'),
     path('musicians/', MusicianList.as_view(), name='musicians_page_url'),
-    path('musical-instruments/', MusicalInstrumentList.as_view(), name='musical_instruments_page_url'),
-    path('contacts/', ContactPage.as_view(), name='contacts_url')
+    # path('musicians/<str:slug>/', MusicianDetail.as_view(), name='musicians_detail_url'),
+    path('news/', NewsList.as_view(), name='news_page_url'),
+    # path('news/<str:slug>/', NewsDetail.as_view(), name='news_detail_url'),
+    path('privacy/', PrivacyPage.as_view(), name='privacy_page_url'),
+    path('songs/', SongPage.as_view(), name='songs_page_url'),
+    path('user-terms/', UserTermsPage.as_view(), name='user_terms_page_url'),
 ]
--- music_portal/views.py
+++ music_portal/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
+class AboutPage(View):
+    def get(self, request):
+        return render(request, 'music_portal/about.html')
+
+
+class ContactPage(View):
+    def get(self, request):
+        return render(request, 'music_portal/contacts.html')
+
+
 class MainPage(View):
     def get(self, request):
         return render(request, 'music_portal/index.html')
-
-
-class NewsList(View):
-    def get(self, request):
-        return render(request, 'music_portal/news.html')
-
-
-class MusicianList(View):
-    def get(self, request):
-        return render(request, 'music_portal/musicians.html')
 
 
 class MusicalInstrumentList(View):
@@ -22,6 +22,26 @@ class MusicalInstrumentList(View):
         return render(request, 'music_portal/musical_instruments.html')
 
 
-class ContactPage(View):
+class MusicianList(View):
     def get(self, request):
-        return render(request, 'music_portal/contacts.html')
+        return render(request, 'music_portal/musicians.html')
+
+
+class NewsList(View):
+    def get(self, request):
+        return render(request, 'music_portal/news.html')
+
+
+class PrivacyPage(View):
+    def get(self, request):
+        return render(request, 'music_portal/privacy.html')
+
+
+class SongPage(View):
+    def get(self, request):
+        return render(request, 'music_portal/songs.html')
+
+
+class UserTermsPage(View):
+    def get(self, request):
+        return render(request, 'music_portal/user_terms.html')
