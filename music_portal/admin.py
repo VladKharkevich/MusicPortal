53066a6f73818cceaf3edbb878831a6cc13688a8
Vlad <kharkevichvlad@gmail.com>
2020-07-09 22:57:09 +0300

    Created models of this site

--- music_portal/admin.py
+++ music_portal/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import News
+from .models import *
 
 
 admin.site.register(News)
+admin.site.register(MusicalInstrument)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Mucisian)
+admin.site.register(Band)
+admin.site.register(VotingBallot)
--- music_portal/models.py
+++ music_portal/models.py
@@ -4,17 +4,25 @@ from django.contrib.postgres.fields import JSONField
 
 class News(models.Model):
     title = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, blank=True, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
     content = models.TextField(null=True, blank=True)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     published = models.DateTimeField(auto_now_add=True, db_index=True)
 
+    def __str__(self):
+        return self.title
 
+
+"""
 class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
+
+    def __str__(self):
+        return '{} {}'.format(self.last_name, self.first_name)
+"""
 
 
 class MusicalInstrument(models.Model):
@@ -25,40 +33,65 @@ class MusicalInstrument(models.Model):
         ('p', 'percussion'),
         ('o', 'other')
     )
-    name = models.CharField(max_length=100)
-    slug = models.SlugField()
-    photo = models.ImageField()
-    type_of_instrument = models.CharField(max_length=1, choices=TYPES_OF_INSTRUMENT)
-    description = models.TextField(max_length=100, blank=True, unique=True)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    photo = models.ImageField(upload_to='musical_instrument_photos')
+    type_of_instrument = models.CharField(max_length=1, choices=TYPES_OF_INSTRUMENT, db_index=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Album(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Song(models.Model):
-    name = models.CharField(max_length=100)
-    album = models.ForeignKey(Album, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(unique=True)
+    songfile = models.FileField(upload_to='song_audio')
+    year_of_creation = models.DateField()
+    album = models.ForeignKey(Album, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
 
 
 class Mucisian(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, db_index=True)
+    last_name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    biography = models.TextField(null=True, blank=True)
     date_of_birth = models.DateField()
     date_of_death = models.DateField()
-    biography = models.TextField()
+    photo = models.ImageField(upload_to='musician_photos')
     songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return '{} {}'.format(self.last_name, self.first_name)
 
 
 class Band(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    biography = models.TextField(null=True, blank=True)
     date_of_creation = models.DateField()
     date_of_breakup = models.DateField()
+    photo = models.ImageField(upload_to='band_photos')
     members = models.ManyToManyField(Mucisian)
-    biography = models.TextField()
     songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.title
 
 
 class VotingBallot(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, db_index=True)
     votes = JSONField()
+
+    def __str__(self):
+        return self.title
